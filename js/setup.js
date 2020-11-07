05eb8a70762b6ee044d2519ea48be51dc9f27cb2
Alex <sashaikonnikov55@gmail.com>
2020-11-07 22:35:31 +0700

    Include setup.js in index.html. Added mage generation functionality

--- js/setup.js
+++ js/setup.js
@@ -9,9 +9,51 @@ let template = document.querySelector('#similar-wizard-template').content;
 
 
 let names = ['Иван', 'Хуан Себастья', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-let surname = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-let coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-let eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+let surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+let coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+let eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
+let generateRandomNun = function (max) {
 
+    let randNum = Math.random() * (max + 1);
+    return Math.floor(randNum);
+
+};
+
+let generateValue = function (arr) {
+
+    return arr[ generateRandomNun(arr.length-1) ];
+
+};
+
+let generateArrayWizards = function () {
+    let arrayWizards = [];
+    
+    for (let i = 0; i <= 3; i++) {
+        let wizard = {
+            name: generateValue(names)+ ' ' + generateValue(surnames),
+            coatColor: generateValue(coatColors),
+            eyesColor: generateValue(eyesColors)
+        }
+        arrayWizards.push(wizard);
+    }
+    return arrayWizards;
+};
+
+let generateWizard = function (array) {
+
+    document.querySelector('.setup-similar').classList.remove('hidden');
+    
+    for(let i = 0; i <= array.length - 1; i++) {
+
+        let wizard = template.cloneNode(true);
+        wizard.querySelector('.setup-similar-label').textContent = array[i].name;
+        wizard.querySelector('.wizard-coat').style.fill = array[i].coatColor;
+        wizard.querySelector('.wizard-eyes').style.fill = array[i].eyesColor;
+        pool.appendChild(wizard);
+
+    }
+}
+
+generateWizard(generateArrayWizards());
 
