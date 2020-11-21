0e2e40954bfcdf247546e3f73c523e5e08a619b1
Alex <sashaikonnikov55@gmail.com>
2020-11-21 22:24:08 +0700

    Add the load function.

--- js/backend.js
+++ js/backend.js
@@ -1,15 +1,35 @@
 (function() {
 
-    window.backend = {
+    window.load = function (onError, callback) {
 
-        'load': function(cb, onError) {
-            
-        },
+        let xhr = new XMLHttpRequest ();
+        let URL = "https://javascript.pages.academy/code-and-magick/data";
+        xhr.responseType = 'json';
+        xhr.timeout = 10000;
 
-        'save': function() {
+        xhr.addEventListener('load', function() {
 
-        }
+            if (xhr.status === 200) {
 
-    };
+                callback(xhr.response);
+
+            } else {
+
+                console.log('Упс, кажется что-то пошло не так. Код ошибки' + xhr.status);
+
+            }
+        });
+
+        xhr.addEventListener('error', function() {
+            onError('Упс, кажется что-то пошло не так. Код ошибки: ' + xhr.status);
+        });
+
+        xhr.addEventListener('timeout', function() {
+            onError('Время ответа истекло. Код ошибки: ' + xhr.status);
+        });
+
+        xhr.open('get', URL);
+        xhr.send();
+    }
 
 })();
--- js/setup.js
+++ js/setup.js
@@ -2,69 +2,79 @@
 
 (function() {
 
-let pool = document.querySelector('.setup-similar-list');
-let template = document.querySelector('#similar-wizard-template').content;
-let names = ['Иван', 'Хуан Себастья', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-let surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+    let pool = document.querySelector('.setup-similar-list');
+    let template = document.querySelector('#similar-wizard-template').content;
+    let names = ['Иван', 'Хуан Себастья', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+    let surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 
-let coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-let eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+    let coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+    let eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-let generateRandomNun = function (max) {
+    let generateRandomNun = function (max) {
 
-    let randNum = Math.random() * (max + 1);
-    return Math.floor(randNum);
+        let randNum = Math.random() * (max + 1);
+        return Math.floor(randNum);
 
-};
+    };
 
-let selectRandomValue = function (arr) {
+    let selectRandomValue = function (arr) {
 
-    return arr[ generateRandomNun(arr.length-1) ];
+        return arr[ generateRandomNun(arr.length-1) ];
 
-};
+    };
 
-let createArrayWizards = function () {
 
-    let arrayWizards = [];
-    
-    for (let i = 0; i <= 3; i++) {
 
-        let wizard = {
-            name: selectRandomValue(names) + ' ' + selectRandomValue(surnames),
-            coatColor: selectRandomValue(coatColors),
-            eyesColor: selectRandomValue(eyesColors)
+    /*let createArrayWizards = function () {
+
+        let arrayWizards = [];
+        
+        for (let i = 0; i <= 3; i++) {
+
+            let wizard = {
+                name: selectRandomValue(names) + ' ' + selectRandomValue(surnames),
+                coatColor: selectRandomValue(coatColors),
+                eyesColor: selectRandomValue(eyesColors)
+            }
+
+            arrayWizards.push(wizard);
+
         }
 
-        arrayWizards.push(wizard);
+        return arrayWizards;
+
+    };*/
+
+    let showWizards = function (array) {
+
+        document.querySelector('.setup-similar').classList.remove('hidden');
+        
+        for(let i = 0; i <= 3; i++) {
+
+            let wizard = template.cloneNode(true);
+            wizard.querySelector('.setup-similar-label').textContent = array[i].name;
+            wizard.querySelector('.wizard-coat').style.fill = array[i].colorCoat;
+            wizard.querySelector('.wizard-eyes').style.fill = array[i].colorEyes;
+            pool.appendChild(wizard);
+
+        }
+
+    };
+
+    let onError = function (message) {
+
+        console.log(message);
 
     }
 
-    return arrayWizards;
+    /*showWizards(createArrayWizards());*/
 
-};
+    window.setup = {
+        'coatColors': coatColors,
+        'eyesColors': eyesColors,
+        'generateRandomNun': generateRandomNun
+    };
 
-let showWizards = function (array) {
-
-    document.querySelector('.setup-similar').classList.remove('hidden');
-    
-    for(let i = 0; i <= array.length - 1; i++) {
-
-        let wizard = template.cloneNode(true);
-        wizard.querySelector('.setup-similar-label').textContent = array[i].name;
-        wizard.querySelector('.wizard-coat').style.fill = array[i].coatColor;
-        wizard.querySelector('.wizard-eyes').style.fill = array[i].eyesColor;
-        pool.appendChild(wizard);
-
-    }
-
-};
-
-showWizards(createArrayWizards());
-
-window.setup = {
-    'coatColors': coatColors,
-    'eyesColors': eyesColors,
-    'generateRandomNun': generateRandomNun
-};
+    window.load(onError, showWizards)
 
 })();
